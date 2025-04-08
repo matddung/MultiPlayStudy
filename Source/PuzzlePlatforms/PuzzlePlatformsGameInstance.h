1c29615142ac22c2bdba7e21dff4da073a158835
matddung <matddung1111@gmail.com>
2025-04-08 17:18:42 +0900

    studying GameInstance

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.h
@@ -13,5 +13,15 @@ UCLASS()
 class PUZZLEPLATFORMS_API UPuzzlePlatformsGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer);
 	
+	virtual void Init() override;
+
+	UFUNCTION(Exec)
+		void Host();
+
+	UFUNCTION(Exec)
+		void Join(const FString& Address);
 };
